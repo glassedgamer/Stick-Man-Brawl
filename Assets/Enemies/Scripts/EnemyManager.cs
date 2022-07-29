152a355473e7b5581407884a59bc95db70c21139
glassedgamer <glassedgamer05@gmail.com>
2022-07-29 13:41:45 -0400

    Enemy Spawner
    Added a basic enemy spawner. Spawns 5 random enemies out of an array after 3 seconds have passed by.

--- Assets/Enemies/Scripts/EnemyManager.cs
+++ Assets/Enemies/Scripts/EnemyManager.cs
@@ -6,4 +6,26 @@ public class EnemyManager : MonoBehaviour {
 
     public GameObject[] enemies;
 
+    public float waveBreak = 1f;
+
+    void Start() {
+        StartCoroutine(EnemyWave());
+    }
+
+    void RandomizeEnemies() {
+        int randomIndex = Random.Range(0, enemies.Length);
+
+        float randomLocationX = Random.Range(0f, 8f);
+        float randomLocationY = Random.Range(-1.92f, -4f);
+        GameObject clone = Instantiate(enemies[randomIndex], new Vector2(randomLocationX, randomLocationY), Quaternion.identity);
+    }
+
+    IEnumerator EnemyWave() {
+        yield return new WaitForSeconds(waveBreak);
+
+        for(int i = 0; i < 5; i++) {
+            RandomizeEnemies();
+        }
+    }
+
 }
